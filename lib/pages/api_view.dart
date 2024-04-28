9a363991f5b20b383f7fe153f330fffdfaa2a7a3
Celocious Altrin <celociousalltrinmf@gmail.com>
2024-04-28 20:32:12 +0530

    style added

--- lib/pages/api_view.dart
+++ lib/pages/api_view.dart
@@ -48,17 +48,28 @@ class _ApiViewPageState extends State<ApiViewPage> {
                     fontWeight: FontWeight.bold),
               ),
             )
-          : Column(
-              children: [
-                Text(
-                  todo["title"],
-                  style: const TextStyle(color: Colors.black),
-                ),
-                Text(
-                  todo["description"],
-                  style: const TextStyle(color: Colors.black),
-                )
-              ],
+          : Padding(
+              padding: const EdgeInsets.all(18.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 20),
+                    child: Text(
+                      todo["title"],
+                      style: const TextStyle(
+                          color: Colors.black,
+                          fontSize: 36,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Text(todo["description"],
+                      style: const TextStyle(
+                        color: Colors.black,
+                        fontSize: 26,
+                      ))
+                ],
+              ),
             ),
     );
   }
